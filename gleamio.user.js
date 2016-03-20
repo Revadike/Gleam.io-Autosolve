83c63f355bd306952d3b7e318de2644d5000b503
Royalgamer06 <royalgamer06@hotmail.com>
2016-03-20 21:32:54 +0100

    Added steam group autojoin (may need improvements)

--- gleamio.user.js
+++ gleamio.user.js
@@ -1,32 +1,43 @@
 // ==UserScript==
 // @name         Gleam.io Autosolve
 // @namespace    GLEAM
-// @version      1.4
+// @version      1.5
 // @description  lets save some time
-// @author       Tackyou
+// @author       Tackyou & Royalgamer06
 // @license      https://raw.githubusercontent.com/Tackyou/Gleam.io-Autosolve/master/LICENSE
 // @icon         http://i.imgur.com/6PuVE2l.png
-// @match        *gleam.io/*
+// @include      *gleam.io/*
+// @include      *://steamcommunity.com/groups/*
 // @supportURL   https://github.com/Tackyou/Gleam.io-Autosolve/issues
 // @updateURL    https://raw.githubusercontent.com/Tackyou/Gleam.io-Autosolve/master/gleamio.user.js
 // @downloadURL  https://raw.githubusercontent.com/Tackyou/Gleam.io-Autosolve/master/gleamio.user.js
 // @grant        none
 // ==/UserScript==
 
-var wo = window.open;
-window.open = function(){
-   return;
-}
-var processed = [];
-console.log('[GLEAM] Welcome!');
-$(function(){
-    gleam();
-    if ($("#current-entries") !== null) {
-        $('.span4.blue-square.ng-scope').after('<div class="span4 green-square ng-scope"><span class="square-describe mont"><span class="status small"><span class="current ng-binding" id="winning-chance">NaN</span></span><span class="description ng-binding">Winning Chance</span></span></div>');
-        $("div.square-row.row-fluid.center.ng-scope > .span4").width('25%');
-        setTimeout(setChance, 400);
+var name1 = location.href.split('?ref=')[0];
+if(name1.indexOf('steamcommunity.com') > -1){
+    if(location.href.indexOf("?ref=") > -1){
+        setTimeout(function(){
+            document.forms['join_group_form'].submit();
+            setTimeout(function(){ location.href = decodeURIComponent(location.href.split('?ref=')[1])+ '?ref='+location.href; },1000);
+        }, 2000);
     }
-});
+}else if(name1.indexOf('gleam.io') > -1){
+    var wo = window.open;
+    window.open = function(){
+        return;
+    }
+    var processed = [];
+    console.log('[GLEAM] Welcome!');
+    $(function(){
+        gleam();
+        if ($("#current-entries") !== null) {
+            $('.span4.blue-square.ng-scope').after('<div class="span4 green-square ng-scope"><span class="square-describe mont"><span class="status small"><span class="current ng-binding" id="winning-chance">NaN</span></span><span class="description ng-binding">Winning Chance</span></span></div>');
+            $("div.square-row.row-fluid.center.ng-scope > .span4").width('25%');
+            setTimeout(setChance, 400);
+        }
+    });
+}
 function gleam(){
     if($("div.redeem-container").is(':visible')){
         console.log('[GLEAM] already completed');
@@ -58,6 +69,12 @@ function gleam(){
                 window.open = wo;
             }
         }, 1000);
+    }
+    var steamgroups = $('a[data-track-event="###APP_NAME### Click|steam|join_group"]');
+    for (var i = 0; i < steamgroups.length; i++) {
+        if (steamgroups[i].querySelector(".ng-binding.ng-scope.ng-hide") === null) { //if not completed yet
+            location.href = steamgroups[i].querySelector("span > div > a").innerHTML + "?ref=" + location.href;
+        }
     }
 }
 function checkStatus(elem){
